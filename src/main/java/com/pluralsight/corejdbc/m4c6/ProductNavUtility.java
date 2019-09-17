801ef7853cc742aa7ba865997657cbc83a70ec13
uncommon-design <richard@uncommon-design.com>
2019-09-17 06:00:10 -0500

    added class for m4c6

--- src/main/java/com/pluralsight/corejdbc/m4c6/ProductNavUtility.java
+++ src/main/java/com/pluralsight/corejdbc/m4c6/ProductNavUtility.java
@@ -18,44 +18,35 @@ public class ProductNavUtility {
 		}
 	}
 	
-	public void printFirst() throws Exception{
-		
+	public void printFirst() throws Exception{		
 		if(resultSet.first()) {
 			String name = resultSet.getString("productName");
 			System.out.println("   " + name);
 		}
-
 	}
 	
-	public void printLast() throws Exception{
-		
+	public void printLast() throws Exception{	
 		if(resultSet.last()) {
 			String name = resultSet.getString("productName");
 			System.out.println("   " + name);
 		}
-
 	}
 
-	public void printAt(int postion) throws Exception{
-		
+	public void printAt(int postion) throws Exception{		
 		if(resultSet.absolute(postion)) {
 			String name = resultSet.getString("productName");
 			System.out.println("   " + name);
 		}
-
 	}
 
-	public void printRelative(int postion) throws Exception{
-		
+	public void printRelative(int postion) throws Exception{		
 		if(resultSet.relative(postion)) {
 			String name = resultSet.getString("productName");
 			System.out.println("   " + name);
 		}
-
 	}
 
 	public void printReverse() throws Exception {
-
 		resultSet.afterLast();
 		while (resultSet.previous()) {
 			String name = resultSet.getString("productName");
